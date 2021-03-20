8471f8ab8c2b2bd7b6a8d9b5de6d5b9412bc1919
Damian Juszczak <damianjk0508@gmail.com>
2021-03-20 18:15:44 +0100

    Add basic frontend

--- website/webcomic/urls.py
+++ website/webcomic/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'webcomic'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.index, name='index'),
+    path('<str:language>/<int:episode>/<int:page>/', views.select, name='select'),
 ]
--- website/webcomic/views.py
+++ website/webcomic/views.py
@@ -1,6 +1,12 @@
-from django.views.generic.base import TemplateView
+# from django.views.generic.base import TemplateView
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-class IndexView(TemplateView):
-    template_name = 'webcomic/index.html'
+def index(request):
+    return render(request, "webcomic/index.html")
+
+def select(request, language, episode, page, template_name="index.html"):
+    path_str = 'kot'
+    return render(request, template_name, {'path': path_str})
+

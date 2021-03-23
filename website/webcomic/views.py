36e04a6d0e26cb7d201658a3ec5f9816ee8ee494
Damian Juszczak <damianjk0508@gmail.com>
2021-03-23 18:01:48 +0100

    Change project structure

--- website/webcomic/views.py
+++ website/webcomic/views.py
@@ -1,12 +1,21 @@
 # from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def index(request):
-    return render(request, "webcomic/index.html")
+    response = redirect('/pl/1/0')
+    return response
 
-def select(request, language, episode, page, template_name="index.html"):
-    path_str = 'kot'
-    return render(request, template_name, {'path': path_str})
+def select(request, language, episode, page):
+    path_str = '/media/'
+    if language == 'pl':
+        path_str += 'PL_'
+    else:
+        path_str += 'EN_'
+    
+    path_str += f'{episode}_{page}.png'
+
+    return render(request, "webcomic/index.html", {'path': path_str, 'episode': episode, 'page': page})
 

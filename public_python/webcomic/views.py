88c04a99b543137335ded3aeaf45087f1d562a2d
Damian Juszczak <damianjk0508@gmail.com>
2021-04-20 17:03:56 +0200

    Add blockade of pages that don't exist

--- public_python/webcomic/views.py
+++ public_python/webcomic/views.py
@@ -3,12 +3,33 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 
 
+def check(episode, page):
+    max_episode = 2
+    max_page_1 = 46
+    max_page_2 = 0
+    if episode > max_episode:
+        episode = 1
+    if episode <  1:
+        episode = max_episode
+    if episode == 1:
+        if page > max_page_1:
+            page = 0
+            episode += 1
+        if page < 0:
+            page = max_page_1
+    if episode == 2:
+        if page > max_page_2:
+            page = 0
+        if page < 0:
+            page = max_page_2
+    return episode, page
+
 def index(request):
     response = redirect('/pl/1/0')
     return response
 
 def select(request, language, episode, page):
-    path_str = '/media/'
+    path_str = '/media/webcomic/comic_pages/'
     if language == 'pl':
         path_str += 'PL_'
         url_str = '/pl/'
@@ -18,38 +39,30 @@ def select(request, language, episode, page):
 
     if request.is_ajax() and request.method == "GET":
         element_id = request.GET.get('element_id')
-        max_episode = 2
-        max_page = 46
         if element_id == 'next-page':
             page += 1
-            if page > max_page:
-                page = 0
-                episode += 1
 
         elif element_id == 'previous-page':
             page -= 1
-            if page < 0:
-                page = max_page
 
         elif element_id == 'next-episode':
             episode += 1
             page = 0
-            if episode > max_episode:
-                episode = 1
 
         elif element_id == 'previous-episode':
             episode -= 1
             page = 0
-            if episode <  1:
-                episode = max_episode
         else:
             pass
+        
+        episode, page = check(episode, page)
 
         path_str += f'{episode}_{page}.png'
         url_str += f'{episode}/{page}/'
 
         return JsonResponse({'path': path_str, 'new_url': url_str, 'episode':episode, 'page': page}, status=200)
     else:
+        episode, page = check(episode, page)
         path_str += f'{episode}_{page}.png'
 
         if request.user_agent.is_mobile or request.user_agent.is_tablet:

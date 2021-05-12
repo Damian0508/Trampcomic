631b250c05c361737931a11e00a501d95da7ff91
Damian Juszczak <damianjk0508@gmail.com>
2021-05-12 16:36:16 +0200

    Fix page select system bug

--- public_python/webcomic/views.py
+++ public_python/webcomic/views.py
@@ -3,34 +3,41 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Episode, Page
 
-
-def controller(episode, page, ep_lang_str):
-    try:
-        nr_of_episodes = Episode.objects.filter(counted=True).count()
-        if episode > nr_of_episodes:
-            episode = 1
-        if episode <  1:
-            episode = nr_of_episodes
-        
+def check(episode, ep_lang_str):
+    try:    
         selected_episode = Episode.objects.get(name=f'{ep_lang_str}{episode}')
         nr_of_pages = Page.objects.filter(episode=selected_episode).count()
-        
-        if page > nr_of_pages:
+    except:
+        nr_of_pages = 1
+    return nr_of_pages
+
+
+def controller(episode, page, ep_lang_str):
+    
+    nr_of_episodes = Episode.objects.filter(counted=True).count()
+    if episode > nr_of_episodes:
+        episode = 1
+    if episode <  1:
+        episode = nr_of_episodes
+    nr_of_pages = check(episode, ep_lang_str)
+    
+    if page > nr_of_pages - 1:
+        page = 0
+        episode += 1
+    if page < 0:
+        if episode > 1:
+            episode -= 1
+            nr_of_pages = check(episode, ep_lang_str)
+            page = nr_of_pages - 1
+        else:
             page = 0
-            episode += 1
-        if page < 0:
-            if episode > 1:
-                episode -= 1
-                selected_episode = Episode.objects.get(name=f'{ep_lang_str}{episode}')
-                nr_of_pages = Page.objects.filter(episode=selected_episode).count()
-                page = nr_of_pages
-            else:
-                page = 0
+    nr_of_pages = check(episode, ep_lang_str)
+
+    try:
         comic_page = Page.objects.get(name=f'{ep_lang_str}{episode}_{page}.png')
-        path_str = comic_page.image.url
     except:
         comic_page = Page.objects.get(name=f'{ep_lang_str}404.png')
-    path_str = comic_page.image.url   
+    path_str = comic_page.image.url
     return episode, page, nr_of_pages, path_str
 
 def index(request):
@@ -69,16 +76,16 @@ def select(request, language, episode, page):
         url_str += f'{episode}/{page}/'
 
         return JsonResponse({'path': path_str, 'new_url': url_str,
-            'episode':episode, 'page': page, 'nr_of_pages': nr_of_pages,
+            'episode':episode, 'page': page, 'nr_of_pages': nr_of_pages-1,
             'hero_path': hero_path}, status=200)
     else:
         episode, page, nr_of_pages, path_str = controller(episode, page, ep_lang_str)
         if request.user_agent.is_mobile or request.user_agent.is_tablet:
             return render(request, "webcomic/mobile.html", {'path': path_str,
-                'episode': episode, 'page': page, 'nr_of_pages': nr_of_pages,
+                'episode': episode, 'page': page, 'nr_of_pages': nr_of_pages-1,
                 'hero_path': hero_path})
         else:
             return render(request, "webcomic/desktop.html", {'path': path_str,
-                'episode': episode, 'page': page, 'nr_of_pages': nr_of_pages,
+                'episode': episode, 'page': page, 'nr_of_pages': nr_of_pages-1,
                 'hero_path': hero_path})
 
